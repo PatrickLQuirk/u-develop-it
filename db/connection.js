a8685395767b4a0b139cc67caae38549a85db607
Patrick Quirk <patrickliamquirk@yahoo.com>
2022-04-28 20:30:56 -0500

    add and get votes

--- db/connection.js
+++ db/connection.js
@@ -7,10 +7,10 @@ const db = mysql.createConnection(
     {
         host: 'localhost',
         // Your MySQL username,
-        user: process.env.DB_NAME,
+        user: process.env.DB_USER,
         // Your MySQL password
-        password: process.env.DB_USER,
-        database: process.env.DB_PW
+        password: process.env.DB_PW,
+        database: process.env.DB_NAME
     },
     console.log('Connected to the election database.')
 );
